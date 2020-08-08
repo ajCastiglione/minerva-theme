b30d3549dc34270f526a65a864006dd5ac61838e
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-08-08 10:43:49 -0400

    updated blocks location for new template layout

--- theme/acf-blocks.php
+++ theme/acf-blocks.php
@@ -23,7 +23,7 @@ function my_acf_block_render_callback($block)
     $slug = str_replace('acf/', '', $block['name']);
 
     // include a template part from within the "template-parts/blocks" folder
-    $path = get_template_directory() . "/template-parts/blocks/content-{$slug}.php";
+    $path = get_template_directory() . "/library/blocks/{$slug}/{$slug}.php";
     if (file_exists($path)) {
         include($path);
     }
